6c9f503065018588a4790477094b56ccd921fe87
alaa <alaa2amz@gmail.com>
2024-07-13 13:02:48 +0300

    till-up-handle

--- service/post/post.go
+++ service/post/post.go
@@ -5,6 +5,7 @@
 // holds model, web , api ,test.
 // TODO:generic joiner
 // TODO:edit and new points
+// TODO:validate basic data types on create
 package post
 
 import (
@@ -13,6 +14,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"reflect"
+	//"encoding/json"
 
 	"github.com/alaa2amz/g1/mw"
 	"github.com/alaa2amz/g1/service"
@@ -24,8 +27,8 @@ import (
 
 var (
 	DB           *gorm.DB
-	Path         string = "/post"
-	Editions            = []string{"", "api"}
+	Path         string = "/g1/post"
+	Editions            = []string{"", "/api"}
 	TemplatePath        = "post/template"
 )
 
@@ -57,7 +60,7 @@ var relations = map[string]string{
 }
 
 type Post struct {
-	ID       uint     `form:"id" gorm:"primaryKey"`
+	ID       uint     `form:"id" gorm:"primaryKey"`//id should be removed from form
 	Title    string   `form:"title" binding:"required"`
 	Descript *string  `form:"descript"`
 	Content  string   `form:"content" gorm:"default:null;not null"`
@@ -68,9 +71,9 @@ type Post struct {
 	//Tag      *Tag     `form:"tagname"`
 	//Tag         *Tag       `form:"tag"`
 	PublishAt     *time.Time `form:"publish"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
+//	CreatedAt   time.Time
+//	UpdatedAt   time.Time
+//	DeletedAt   time.Time
 	//gorm.Model
 
 }
@@ -105,8 +108,8 @@ func Init() {
 func Register(r *gin.Engine) *gin.Engine {
 	r.LoadHTMLGlob("**/post/template/*.tmpl")
 
-	for _, ed := range Editions {
-		fullPath := ed + Path
+	for _, edition := range Editions {
+		fullPath := edition + Path
 		//end points in CRUD order
 		// cr -> create
 		// rt -> retrive
@@ -121,11 +124,12 @@ func Register(r *gin.Engine) *gin.Engine {
 		r.GET(fullPath, mw.KissAuth,rt)
 		r.GET(fullPath+"/:id", gt)
 		r.PUT(fullPath+"/:id", up)
+		r.POST(fullPath+"/:id", up)
 		r.DELETE(fullPath+"/:id", dl)
 
 		//r.GET(fullPath+"/test", tst)
-		//r.GET(fullPath+"/new", nw)
-		//r.GET(fullPath+"/:id/edit", ed)
+		r.GET(fullPath+"/new", nw)
+		r.GET(fullPath+"/:id/edit", ed)
 	}
 	return r
 }
@@ -143,6 +147,7 @@ func send(r *Reply, c *gin.Context) {
 		c.JSON(r.StatusCode, gin.H{"data": r.Data, "error": errMsg})
 		return
 	} else {
+		//TODO:handle templates errors and unfound templates or empty templates
 		c.HTML(r.StatusCode, r.Template, gin.H{"data": r.Data, "error": errMsg})
 		return
 	}
@@ -215,13 +220,17 @@ func cr(c *gin.Context) {
 }
 
 func rt(c *gin.Context) {
+	//prototypes -> ps
+	ps := Protos()
+	//results map
+	rm := &[]map[string]any{}
+	r := &Reply{}
 	//get query string
 	qs := c.Request.URL.Query()
 	//parse query to term struct slice
 	terms := parseQueryString(qs)
 	//debug
-	log.Printf("%+v\n", terms)
-	ps := Protos()
+	log.Printf("terms: %+v\n", terms)
 	//start session QDB
 	QDB := DB.Session(&gorm.Session{})
 	QDB.Model(&ps)
@@ -241,9 +250,38 @@ func rt(c *gin.Context) {
 		}
 	}
 	log.Println(QDB.ToSQL(func(q *gorm.DB) *gorm.DB { return q.Find(&ps) }))
-	QDB.Find(&ps)
-	//c.JSON(200, gin.H{"data": ps})
-	//send(Result{200,
+	//result := QDB.Find(&ps)
+	result := QDB.Model(&ps).Find(rm)
+//	result := QDB.Find(&ps)
+	//mid,err := json.Marshal(&ps)
+	//json.Unmarshal(mid,rm)
+	//log.Println(err.Error())
+	if result.Error != nil {
+		r.StatusCode = 500
+		r.Error = result.Error
+		r.Template = "error.tmpl"
+		send(r,c)
+		return
+	}
+	r.StatusCode = 200
+	//frm: filtered/form results map
+	frm :=[]map[string]any{}
+	formKeys,err := StructFields(Proto(),"form")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i,_ := range *rm {
+			amap := map[string]any{}
+		for _,k := range formKeys {
+			amap[k]=(*rm)[i][k]
+		}
+		frm=append(frm,amap)
+	}
+	//r.Data = gin.H{"frm":frm,"apath":"dgdggd"}
+	r.Data = gin.H{"frm":frm,"path":c.FullPath()}
+	r.Template = "results.tmpl"
+	send(r,c)
+	
 }
 
 func gt(c *gin.Context) {
@@ -272,6 +310,7 @@ func up(c *gin.Context) {
 	}
 	p.ID = uint(uintID)
 	DB.Save(&p)
+	//TODO:use send rc
 	c.JSON(200, gin.H{"data": p})
 }
 
@@ -291,7 +330,66 @@ func dl(c *gin.Context) {
 	DB.Delete(&p, id)
 	c.JSON(200, gin.H{"data": p})
 }
+func nw(c *gin.Context) {
+	p := Proto()
+	r := &Reply{}
+	formValues,err := StructFields(p,"form")
+	if err != nil {
+		r.StatusCode=400
+		r.Error = err
+		r.Template ="error.tmpl"
+		send(r,c)
+		return
+	}
+	r.StatusCode = 200
+	r.Data = formValues
+	r.Template = "new.tmpl"
+	//c.JSON(200,formValues)
+	send(r,c)
+	return
+}
+func StructFields(aStruct any,aKey string) ([]string,error) {
+	values := []string{}
+	typ := reflect.TypeOf(aStruct)
+	if typ.Kind() != reflect.Struct {
+		    return nil, fmt.Errorf("%s is not a struct", typ)
+	    }
+	    for i := 0; i < typ.NumField(); i++ {
+		        fld := typ.Field(i)
+			    if val := fld.Tag.Get(aKey); val != "" { 
+				            values = append(values,val)
+					        }
+					}
+					return values, nil}
 
+
+func ed(c *gin.Context) {
+	p := Proto()
+	r := &Reply{}
+	m := map[string]any{}
+	fm := map[string]any{}
+	//TODO:handle errorr
+	id:=c.Param("id")
+	DB.Model(&p).First(&m,id)
+	formValues,err := StructFields(p,"form")
+	if err != nil {
+		r.StatusCode=400
+		r.Error = err
+		r.Template ="error.tmpl"
+		send(r,c)
+		return
+	}
+	for _,k := range formValues {
+		fm[k]=m[k]
+	}
+	r.StatusCode = 200
+	r.Data = gin.H{"fm":fm,"path":c.Request.URL.EscapedPath(),"id":id}
+	log.Printf("%+v\n",fm)
+	r.Template = "edit.tmpl"
+	//c.JSON(200,formValues)
+	send(r,c)
+	return
+}
 //result := DB.First(&p, id)
 //c.JSON(200, gin.H{"data": p})
 //result := DB.Model(&p).Where("ID = ?", id).First(&m)
